4e1e3eb54180f8005addc316af0caa351b2816b5
ka-os <144249152+ka-os@users.noreply.github.com>
2024-05-08 18:06:54 +0200

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
+const Home = () => {
+
+    const [blogs, setBlogs] = useState([
+        { title: 'My new website', body: 'lorem ipsum 1...', author: 'mario', id: 1 },
+        { title: 'Welcome party!', body: 'lorem ipsum 2...', author: 'yoshi', id: 2 },
+        { title: 'Web dev top tips', body: 'lorem ipsum 3...', author: 'mario', id: 3 },
+        { title: 'My old website', body: 'lorem ipsum 4...', author: 'mario', id: 4 },
+        { title: 'Welcome to afterparty!', body: 'lorem ipsum 5...', author: 'yoshi', id: 5 },
+        { title: 'Web dev top 10 tips', body: 'lorem ipsum 6...', author: 'mario', id: 6 }
+    ]);
+    
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+    
+
+    return (
+        <div className="home">
+            <BlogList blogs={blogs} title="All blogs !!!" handleDelete={handleDelete}/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs !!!"/>                                  
+        </div>
+    );
+}
+ 
+export default Home;
